8aed3c2fb0232b1a249bc449f673011b09b385d9
Gileandro Campos <gileandrocampos8@gmail.com>
2024-08-04 15:48:25 -0300

    criar método pool para gerenciar conexões do banco de dados

---
+++ src/database/pool.js
@@ -0,0 +1,12 @@
+import pkg from 'pg';
+const { Pool } = pkg;
+
+const pool = new Pool({
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  database: process.env.PGDATABASE,
+});
+
+export default pool
