58497b68ff324d9c33e205688fd634a40ae50974
Gileandro Campos <gileandrocampos8@gmail.com>
2024-08-30 07:26:17 -0300

    remover método de encryptar senha do bloco de usuário

--- src/service/user.js
+++ src/service/user.js
@@ -90,16 +90,4 @@ export class User {
 
     return { status: 200 };
   }
-
-  async encryptPassword(password) {
-    const saltRounds = 10;
-    try {
-      const salt = await bcrypt.genSalt(saltRounds);
-      const hashedPassword = await bcrypt.hash(password, salt);
-      return hashedPassword;
-    } catch (error) {
-      console.error("Error encrypting password:", error);
-      throw error;
-    }
-  }
 }
